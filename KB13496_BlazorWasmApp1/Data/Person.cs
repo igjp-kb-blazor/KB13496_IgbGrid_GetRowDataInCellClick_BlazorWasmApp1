4ce780d30b3ea51be3e6ff6bdae7a553b6ee6e51
norikois <33947244+norikois@users.noreply.github.com>
2024-04-23 17:29:22 +0900

    初回コミット

---
+++ KB13496_BlazorWasmApp1/Data/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace KB13496_BlazorWasmApp1.Data;
+
+public class Person
+{
+    public int ID { get; set; }
+    public String FamilyName { get; set; } = String.Empty;
+    public String GivenName { get; set; } = String.Empty;
+    public String Prefecture { get; set; } = String.Empty;
+    public String City { get; set; } = String.Empty;
+
+    public Person()
+    {
+    }
+}
---
+++ KB13496_BlazorWasmApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using IgniteUI.Blazor.Controls;
+using KB13496_BlazorWasmApp1;
+using KB13496_BlazorWasmApp1.Data;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor(typeof(IgbGridModule));
+builder.Services.AddSingleton<PeopleDataService>();
+
+await builder.Build().RunAsync();
